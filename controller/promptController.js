4d826455ef278e761f5b24dc11484faec5eacb76
Rani Giro <raniking15@gmail.com>
2025-01-04 22:25:19 +0200

    Feat: Added docker integration - DB + server

--- controller/promptController.js
+++ controller/promptController.js
@@ -1,29 +1,27 @@
-import OpenAI from 'openai'
+import OpenAI from "openai";
 
-export async function handlePrompt(req,res){
-    const api_key = process.env.OPEN_AI_KEY
-    try{
-        const prompt = req.body.prompt;
+export async function handlePrompt(req, res) {
+  const api_key = process.env.OPEN_AI_KEY;
+  try {
+    const prompt = req.body.prompt;
 
-        const openai = new OpenAI({apiKey:api_key});
-        const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-                { role: "system", content: "Make all output in json." },
-                {
-                    role: "user",
-                    content: prompt,
-                },
-            ],
-        })
+    const openai = new OpenAI({ apiKey: api_key });
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "Make all output in json." },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
 
-        console.log(completion.choices[0].message)
-        res.status(200).send();
-    }catch(error){
-        res.status(400).json({
-            "error":error.error
-        })
-    }
-    
+    console.log(completion.choices[0].message);
+    res.status(200).send(completion.choices[0].message);
+  } catch (error) {
+    res.status(400).json({
+      error: error.error,
+    });
+  }
 }
-
