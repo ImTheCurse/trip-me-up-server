8a489f2266b90ce1ebeef4b7684ddf2edcfc0523
rani <raniking15@gmail.com>
2025-01-24 15:36:06 +0200

    Bug Fix: fixed cors issue.

--- index.js
+++ index.js
@@ -22,7 +22,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-	res.set('Access-Control-Allow-Origin', '*');
+
+	const allowedOrigins = [
+		"http://localhost:3000", // Frontend development port(not sure if needed...)
+        `http://localhost:${port}`, // Local development
+        "https://trip-me-up-server.onrender.com", // Production client URL
+    ];
+
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.set("Access-Control-Allow-Origin", origin);
+    }
 	res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	res.set('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
 	res.set('Content-Type', 'application/json');
