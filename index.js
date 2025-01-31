d4673a2d542b4f5fe812877401fb0af74084fc18
rani <raniking15@gmail.com>
2025-01-31 16:46:03 +0200

    Updated allowed origin

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 		"https://localhost:3000", // Frontend development port(not sure if needed...)
 		"https://localhost:5173",
         `https://localhost:${port}`, // Local development
+		`https://trip-me-up-client.onrender.com`
     ];
 
     const origin = req.headers.origin;
