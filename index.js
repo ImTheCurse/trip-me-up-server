c1bea2b144eb6bda6ea114ce973ef926bbcd6b9e
Daniella <daniellaelbaz711@gmail.com>
2026-01-16 21:20:36 +0200

    change render client

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     "https://localhost:3000", // Frontend development port(not sure if needed...)
     "https://localhost:5173",
     `https://localhost:${port}`, // Local development
-    `https://trip-me-up-client.onrender.com`,
+    `https://trip-me-up-client-cr26.onrender.com`,
   ];
 
   const origin = req.headers.origin;
